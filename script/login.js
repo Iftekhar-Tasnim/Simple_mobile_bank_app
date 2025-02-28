3d2a421487be1dcd6b54654119da6af1a69fcff1
Iftekhar Tasnim <iftekhartasnim@gmail.com>
2025-02-28 18:56:36 +0600

    Login Page complete

--- script/login.js
+++ script/login.js
@@ -1,3 +1,20 @@
-document.getElementById('login-btn').addEventListener('click',function(){
-    
-})
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const AccountNumber = document.getElementById("account-no").value;
+    console.log(AccountNumber);
+
+    const pinNumber = document.getElementById("pin-no").value;
+    console.log(pinNumber);
+
+    if (AccountNumber.length === 11) {
+      if (pinNumber == 1234) {
+        window.location.href="./main.html"
+      } else {
+        alert("Invalid Pin");
+      }
+    } else {
+      alert("Invaid No");
+    }
+  });
